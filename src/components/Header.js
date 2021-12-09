429691caf7b5a2cbb25b759658cf8b7ce3d697d5
tchoune <contact@yannickperret.com>
2021-12-09 14:37:10 +0100

    delete space rigth screen and put Work in progress right

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,18 +4,18 @@ import email from '../assets/images/email.png';
 
 const Header = () => {
     return (
-        <div className="header">
-                <div className="header__logo">
-                    <Link to="/" >Yannick Perret</Link>
-                </div>
-                <div className="header__mail">
-                <img src={email} alt="icone pour envoyer un email" /><p> Contact@YannickPerret.com </p>
-                </div>
-
-                <div className='workInProgress'>
-                    Work in progess !  :)
-                </div>
-        </div>
+        <>
+            <div className="header">
+                    <div className="header__logo">
+                        <Link to="/" >Yannick Perret</Link>
+                    </div>
+                    <div className="header__mail">
+                    <img src={email} alt="icone pour envoyer un email" /><p> Contact@YannickPerret.com </p>
+                    </div>
+            </div>
+            <div className='workInProgress'>
+            </div>
+        </>
     );
 };
 
